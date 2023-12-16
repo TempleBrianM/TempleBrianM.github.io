d7e1dd469e3e4a5d29f5f6d63abd1af03c4d3e07
TempleBrianM <Temple.Brian.M@gmail.com>
2023-12-16 15:37:55 -0500

    Used javascript to set min height of index section.

--- root/scripts/index.js
+++ root/scripts/index.js
@@ -1,4 +1,4 @@
 const index = document.getElementById('index');
 const header = document.getElementById('header');
 
-index.style.height = 'calc(100vh - ' + header.offsetHeight + 'px)';
+index.style.minHeight = 'calc(100vh - ' + header.offsetHeight + 'px)';
