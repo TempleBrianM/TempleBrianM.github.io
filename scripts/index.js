ab321bba3d6a4dcc53613881e03c4a5d1125202c
bmt26 <bmt26@njit.edu>
2023-12-18 13:40:56 -0500

    Custom cursor partially implemented but comment out

--- scripts/index.js
+++ scripts/index.js
@@ -7,14 +7,19 @@ index.style.minHeight = 'calc(100vh - ' + header.offsetHeight + 'px)';
 
 const cursorLeft = document.querySelector('.cursor-left');
 const cursorRight = document.querySelector('.cursor-right');
+const cursorBackgroundLeft = document.querySelector('.cursor-background-left');
+const cursorBackgroundRight = document.querySelector('.cursor-background-right');
 
 // Cursor
+/*
 const positionElement = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
 
-  cursorLeft.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  cursorRight.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  cursorLeft.style.transform = `translate3d(calc(${mouseX}px + 1px), calc(${mouseY}px + 2px), 0)`;
+  cursorRight.style.transform = `translate3d(calc(${mouseX}px + 1px), calc(${mouseY}px + 2px), 0)`;
+  cursorBackgroundLeft.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  cursorBackgroundRight.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
 
-window.addEventListener('mousemove', positionElement)
+window.addEventListener('mousemove', positionElement)*/
