4af219ca5b91ae275a727c3fb9676a76e7e4bf16
bmt26 <bmt26@njit.edu>
2023-12-18 11:11:24 -0500

    Custom cursor partially implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,20 @@
 // TODO better organize javascript functions
+// Header offset
 const index = document.getElementById('index');
 const header = document.getElementById('header');
 
 index.style.minHeight = 'calc(100vh - ' + header.offsetHeight + 'px)';
 
-const cursorSmall = document.querySelector('.small');
-const cursorBig = document.querySelector('.big');
+const cursorLeft = document.querySelector('.cursor-left');
+const cursorRight = document.querySelector('.cursor-right');
 
-
+// Cursor
 const positionElement = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
 
-  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
+  cursorLeft.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  cursorRight.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
 
 window.addEventListener('mousemove', positionElement)
