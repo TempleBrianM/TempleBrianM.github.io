77d04be3aa29976d80931a9338e43d71e2ed8c76
TempleBrianM <Temple.Brian.M@gmail.com>
2023-12-20 16:51:34 -0500

    Changed font, added CRT effect to intro, overhauled intro

--- TODO.md
+++ TODO.md
@@ -2,4 +2,5 @@
 
 - [ ] Upload a smaller headshot photo
 - [ ] Design Web Icon
+- [ ] Credit CRT Display https://aleclownes.com/2017/02/01/crt-display.html
 
--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-// Uses the header's height to set a the index section's minimum height.
-function setMinHeightIndex() {
-    const INDEX = document.getElementById('index');
-    const HEADER = document.getElementById('header');
-
-    INDEX.style.minHeight = 'calc(100vh - ' + HEADER.offsetHeight + 'px)';
-}
