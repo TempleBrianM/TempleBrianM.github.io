a9319e5a64b570080941a717f9bcc051ede7a14d
TempleBrianM <Temple.Brian.M@gmail.com>
2023-12-20 09:53:43 -0500

    Removed TODO

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,3 @@ const index = document.getElementById('index');
 const header = document.getElementById('header');
 
 index.style.minHeight = 'calc(100vh - ' + header.offsetHeight + 'px)';
-
-// Cursor
-/*
-const cursorLeft = document.querySelector('.cursor-left');
-const cursorRight = document.querySelector('.cursor-right');
-const cursorBackgroundLeft = document.querySelector('.cursor-background-left');
-const cursorBackgroundRight = document.querySelector('.cursor-background-right');
-
-const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-
-  cursorLeft.style.transform = `translate3d(calc(${mouseX}px + 1px), calc(${mouseY}px + 2px), 0)`;
-  cursorRight.style.transform = `translate3d(calc(${mouseX}px + 1px), calc(${mouseY}px + 2px), 0)`;
-  cursorBackgroundLeft.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  cursorBackgroundRight.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-}
-
-window.addEventListener('mouse-move', positionElement)*/
