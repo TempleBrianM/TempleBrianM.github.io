e1971934a4e0c4f301658bad706e779e9ed8ced3
TempleBrianM <temple.brian.m@gmail.com>
2024-01-11 10:58:46 -0500

    Scroll-margin-top setting automated

--- TODO.md
+++ TODO.md
@@ -4,6 +4,5 @@
 - [ ] TODO typing sound effect (and monitor turn on?)
 - [ ] Format for phone
 - [ ] Implement contact back-end
-- [ ] scroll-margin-top to .js implement
 - [ ] Footer not at bottom fix
 - [ ] Contact section emojis
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
-/*var sections = document.getElementsByClassName('section')
+var sections = document.getElementsByTagName('section')
+var header = document.getElementsByTagName('header')
 for (var i = 0; i < sections.length; i++) {
-    sections[i].setAttribute("scroll-margin-top", "5em");
-}*/
+    console.log("calc(" + header[0].offsetHeight + "px + " + getComputedStyle(document.body).getPropertyValue('--section-margin') + " - 3px)")
+    sections[i].style.scrollMarginTop = "calc(" + header[0].offsetHeight + "px + " + getComputedStyle(document.body).getPropertyValue('--section-margin') + " - 2px)";
+}
 
 document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
