8c382e736799359450ebb5ef28f9b02cf4bc1ef9
TempleBrianM <temple.brian.m@gmail.com>
2024-01-11 10:22:14 -0500

    Test

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
+/*var sections = document.getElementsByClassName('section')
+for (var i = 0; i < sections.length; i++) {
+    sections[i].setAttribute("scroll-margin-top", "5em");
+}*/
+
 document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
