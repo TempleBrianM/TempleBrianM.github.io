a4f58afa6268b80c2748efa48e6c542c87a5d88e
TempleBrianM <Temple.Brian.M@gmail.com>
2024-01-17 13:36:31 -0500

    Footer is now always at the bottom of the page

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 - [ ] Upload properly colored crt monitor overlay
 - [ ] TODO typing sound effect (and monitor turn on?)
 - [ ] Format for phone
-- [ ] Footer not at bottom fix
 - [ ] Contact section emojis
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
-var sections = document.getElementsByTagName('section')
-var header = document.getElementsByTagName('header')
+var sections = document.getElementsByTagName('section');
+var header = document.getElementsByTagName('header');
+var main = document.getElementsByTagName('main');
+var footer = document.getElementsByTagName('footer');
+
+// Offsets scroll top margin of all sections so as to prevent top from being covered by the header
 for (var i = 0; i < sections.length; i++) {
-    console.log("calc(" + header[0].offsetHeight + "px + " + getComputedStyle(document.body).getPropertyValue('--section-margin') + " - 3px)")
     sections[i].style.scrollMarginTop = "calc(" + header[0].offsetHeight + "px + " + getComputedStyle(document.body).getPropertyValue('--section-margin') + " - 2px)";
 }
 
+// Make page at least as big as the window so that the footer remains at the bottom
+main[0].style.minHeight = "calc(100vh - " + header[0].offsetHeight  + "px - " + getComputedStyle(document.body).getPropertyValue('--section-margin') + " - "+ footer[0].offsetHeight + "px)"
+
+// Sets style.css variable --scrollbar-width to the actual width of the scroll bar
 document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
