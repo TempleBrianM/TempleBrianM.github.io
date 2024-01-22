7c7627b4c5392f8f44de43dadb2b8c5601054aab
TempleBrianM <temple.brian.m@gmail.com>
2024-01-22 11:49:54 -0500

    Header shrinks when not at top of page

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ var sections = document.getElementsByTagName('section');
 var header = document.getElementsByTagName('header');
 var main = document.getElementsByTagName('main');
 var footer = document.getElementsByTagName('footer');
+var topNavLink = document.getElementsByClassName('top-nav-link');
+var logoWordMark = document.getElementsByClassName('logo-word-mark');
 
 // Offsets scroll top margin of all sections so as to prevent top from being covered by the header
 for (var i = 0; i < sections.length; i++) {
@@ -13,3 +15,29 @@ main[0].style.minHeight = "calc(100vh - " + header[0].offsetHeight  + "px - " + 
 
 // Sets style.css variable --scrollbar-width to the actual width of the scroll bar
 document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 50) {
+        for (const i of topNavLink) {
+            i.style.marginTop="0";
+            i.style.marginBottom="0";
+        }
+        for (const i of logoWordMark) {
+            i.style.marginTop="0";
+            i.style.marginBottom="0";
+        }
+    }
+    if (document.documentElement.scrollTop < 50) {
+        for (const i of topNavLink) {
+            i.style.marginTop="revert";
+            i.style.marginBottom="revert";
+        }
+        for (const i of logoWordMark) {
+            i.style.marginTop="revert";
+            i.style.marginBottom="revert";
+        }
+    }
+}
